464696457c16e0932b95358e303cb05c2422f93a
Olivertrain1221 <olivetrain1996@gmail.com>
2022-06-15 17:12:13 +0100

    Add templates and base.html sections

--- candy_cave/urls.py
+++ candy_cave/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
