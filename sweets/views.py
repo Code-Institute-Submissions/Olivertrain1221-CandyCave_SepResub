c631898bf16c04994c62ec3d645e555c8f2ab7d0
Olivertrain1221 <olivetrain1996@gmail.com>
2022-06-28 13:27:49 +0100

    Added search bar functionality to the site

--- sweets/views.py
+++ sweets/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from.models import Sweet
 
 def all_sweets(request):
@@ -6,9 +8,21 @@ def all_sweets(request):
     View that the main sweet page
     """
     sweets = Sweet.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "Please enter something to search")
+                return redirect(reverse('sweets'))
+
+            queries = Q(name__icontains=query) | Q(detail__icontains=query)
+            sweets = sweets.filter(queries)
 
     context = {
         'sweets': sweets,
+        'search_term': query,
     }
     return render(request, 'sweets/sweets.html', context)
 
