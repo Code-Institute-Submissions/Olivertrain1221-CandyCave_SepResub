ee02f1ba3d57888c407d6eca7c25775916879855
Olivertrain1221 <olivetrain1996@gmail.com>
2022-07-01 12:00:44 +0100

    Add categories

--- sweets/views.py
+++ sweets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from.models import Sweet
+from .models import Sweet, Category
 
 def all_sweets(request):
     """
@@ -9,13 +9,20 @@ def all_sweets(request):
     """
     sweets = Sweet.objects.all()
     query = None
-    
+    sweet_type = None
+
     if request.GET:
+        if 'category' in request.GET:
+            sweet_type = request.GET['category']
+            sweets = sweets.filter(category__name__in=sweet_type)
+            sweet_type = Category.objects.filter(name__in=sweet_type)
+            print(sweet_type)
+
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
                 messages.error(request, "Please enter something to search")
-                return redirect(reverse('sweets'))
+                return redirect(reverse('sweets:sweets'))
 
             queries = Q(name__icontains=query) | Q(detail__icontains=query)
             sweets = sweets.filter(queries)
@@ -23,6 +30,7 @@ def all_sweets(request):
     context = {
         'sweets': sweets,
         'search_term': query,
+        'sweet_type': sweet_type,
     }
     return render(request, 'sweets/sweets.html', context)
 
