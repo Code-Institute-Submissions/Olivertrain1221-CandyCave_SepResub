5dbbd4e474e6856f5803cde735805c45d35c5206
Olivertrain1221 <olivetrain1996@gmail.com>
2022-07-05 15:27:57 +0100

    Add bag view and rendering of items

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from sweets.models import Sweet
 
 def basket_items(request):
     """
@@ -9,6 +11,18 @@ def basket_items(request):
     basket_selection = []
     basket_sum = 0
     sweet_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        sweet = get_object_or_404(Sweet, pk=item_id)
+        basket_sum += quantity * sweet.price
+        sweet_count += quantity
+        basket_selection.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'sweet': sweet,
+        })
+
 
     if basket_sum < settings.FREE_POSTAGE_PRICE:
         post = basket_sum * Decimal(settings.DELIVERY_PERCENTAGE / 100)
@@ -16,13 +30,14 @@ def basket_items(request):
     else:
         post = 0
         free_postage_mark = 0
-    
+
     grand_total = post + basket_sum
-    
+
     context = {
         'basket_selection': basket_selection,
         'basket_sum': basket_sum,
         'sweet_count': sweet_count,
+        'post': post,
         'free_postage_mark': free_postage_mark,
         'free_postage_price': settings.FREE_POSTAGE_PRICE,
         'grand_total': grand_total,
--- basket/views.py
+++ basket/views.py
@@ -13,17 +13,16 @@ def add_items_to_basket(request, item_id):
     """
     Adds a users item to there basket
     """
-    quantity_of_product = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         # if already in basket will add whatevery value quantity plus current basket
-        basket[item_id] += quantity_of_product
+        basket[item_id] += quantity
     else:
         # otherwise just adds to basket (what user selects on first add)
-        basket[item_id] = quantity_of_product
+        basket[item_id] = quantity
 
     request.session['basket'] = basket
-    print(request.session['basket'])
     return redirect(redirect_url)
