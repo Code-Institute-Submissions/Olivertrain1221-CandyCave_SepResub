4fd1927d8176deb8cb8b635a1c3dbcccbda7fa9c
Olivertrain1221 <olivetrain1996@gmail.com>
2022-07-11 11:47:30 +0100

    Remove localenv

--- sweets/models.py
+++ sweets/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
@@ -27,7 +27,7 @@ class Sweet(models.Model):
     sku = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
     detail = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    measurement_of_sweet = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
