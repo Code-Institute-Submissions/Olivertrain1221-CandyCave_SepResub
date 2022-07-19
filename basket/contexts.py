b8787f8d99b279a8ec1e9e2dac79dcd89a169eab
Olivertrain1221 <olivetrain1996@gmail.com>
2022-07-19 12:09:01 +0100

    Created checkout app in models

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,11 +3,11 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from sweets.models import Sweet
 
+
 def basket_items(request):
     """
     Shows the basket items available to all templates
     """
-
     sweet_calculated_price = []
     quantity = 0
     basket_selection = []
@@ -18,7 +18,8 @@ def basket_items(request):
     for item_id, item_data in basket.items():
         if isinstance(item_data, int):
             sweet = get_object_or_404(Sweet, pk=item_id)
-            basket_sum += item_data * sweet.price
+            sweet_total = sweet.price * size
+            basket_sum += item_data * sweet_total
             sweet_count += item_data
             basket_selection.append({
                 'item_id': item_id,
@@ -28,17 +29,15 @@ def basket_items(request):
         else:
             for size, quantity in item_data['items_by_size'].items():
                 sweet = get_object_or_404(Sweet, pk=item_id)
-                basket_sum += quantity * sweet.price
+                sweet_total = int(size) * sweet.price
+                basket_sum += quantity * sweet_total
                 sweet_count += quantity
-                print(sweet_count)
-                print(quantity)
                 basket_selection.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'sweet': sweet,
                     'size': size,
                 })
-
 
     if basket_sum < settings.FREE_POSTAGE_PRICE:
         post = basket_sum * Decimal(settings.DELIVERY_PERCENTAGE / 100)
--- basket/views.py
+++ basket/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from sweets.models import Sweet
 
 
 def view_basket(request):
     """
     Shows the basket and whats in it
     """
-
     return render(request, 'basket/basket.html')
 
 
@@ -24,20 +25,87 @@ def add_items_to_basket(request, item_id):
         if item_id in list(basket.keys()):
             if size in basket[item_id]['items_by_size'].keys():
                 basket[item_id]['items_by_size'][size] += quantity
-                # messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {basket[item_id]["sweets_by_size"][size]}')
+                # messages.success(request, f'Updated size {size.upper()}
+                # # {sweet.name} quantity to {basket[item_id]
+                # ["sweets_by_size"][size]}')
             else:
                 basket[item_id]['items_by_size'][size] = quantity
-                # messages.success(request, f'Added size {size.upper()} {product.name} to your basket')
+                # messages.success(request, f'Added size
+                # {size.upper()} {sweet.name} to your basket')
         else:
             basket[item_id] = {'items_by_size': {size: quantity}}
-            # messages.success(request, f'Added size {size.upper()} {product.name} to your basket')
+            # messages.success(request, f'Added size
+            # {size.upper()} {sweet.name} to your basket')
     else:
         if item_id in list(basket.keys()):
             basket[item_id] += quantity
-            # messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+            # messages.success(request, f'Updated
+            # {sweet.name} quantity to {basket[item_id]}')
         else:
             basket[item_id] = quantity
-            # messages.success(request, f'Added {product.name} to your basket')
+            # messages.success(request, f'Added
+            # {sweet.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """Adjust the quantity of the specified sweet to the specified amount"""
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'sweet_measurement' in request.POST:
+        size = request.POST['sweet_measurement']
+    basket = request.session.get('basket', {})
+
+    if size:
+        if quantity > 0:
+            basket[item_id]['items_by_size'][size] = quantity
+            # messages.success(request, f'Updated size {size.upper()}
+            # {product.name} quantity to
+            # {bag[item_id]["items_by_size"][size]}')
+        else:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+            # messages.success(request, f'Removed size
+            # {size.upper()} {sweet.name} from your bag')
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+            # messages.success(request, f'Updated
+            # {sweet.name} quantity to {basket[item_id]}')
+        else:
+            basket.pop(item_id)
+            # messages.success(request, f'Removed {sweet.name} from your bag')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        sweet = get_object_or_404(Sweet, pk=item_id)
+        size = None
+        if 'sweet_measurement' in request.POST:
+            size = request.POST['sweet_measurement']
+        basket = request.session.get('basket', {})
+
+        if size:
+            del basket[item_id]['items_by_size'][size]
+            if not basket[item_id]['items_by_size']:
+                basket.pop(item_id)
+            # messages.success(request, f'Removed size {size.upper()}
+            #  {sweet.name} from your bag')
+        else:
+            basket.pop(item_id)
+            # messages.success(request, f'Removed {sweet.name} from your bag')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        # messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
