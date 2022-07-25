8f630afc9d6a6887913a52a19e0962896a1539ba
Olivertrain1221 <olivetrain1996@gmail.com>
2022-07-25 23:21:21 +0100

    Edit to AWS settings

--- candy_cave/settings.py
+++ candy_cave/settings.py
@@ -164,7 +164,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'candy-cave'
+    AWS_STORAGE_BUCKET_NAME = 'candycave'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
