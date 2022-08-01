98d494c486c238cd33bb227ffd8dd5a1195899a0
unknown <olivertrain1996@gmail.com>
2022-08-01 23:48:09 +0100

    Add site SEO for optimisation in searches

--- candy_cave/urls.py
+++ candy_cave/urls.py
@@ -13,3 +13,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'candy_cave.views.handler404'
