fedd190b6675b479096fb4259e2e42f96e6e5a4d
unknown <olivertrain1996@gmail.com>
2022-09-16 19:27:07 +0100

    Final push

--- candy_cave/settings.py
+++ candy_cave/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", False)
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
+
 
 
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
